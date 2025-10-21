2fbd7d86fc295d9fdd258d7cdaf53377b968dd31
vishnuvb5991-spec <vishnuvb5991@gmail.com>
2025-10-21 04:57:17 -0400

    saved progress

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -12,6 +12,24 @@ const TodoList = () => {
       setHeadingInput('');
     }
   };
+      // Function to handle adding a new list item to a specific todo heading
+const handleAddList = (index) => {
+    // Check if the input for the given index is not empty or just whitespace
+    if (listInputs[index] && listInputs[index].trim() !== '') {
+        const newTodos = [...todos]; // Create a copy of the current todos array
+        newTodos[index].lists.push(listInputs[index]); // Add the new list item to the corresponding heading's list
+        setTodos(newTodos); // Update the todos state with the new list item
+        setListInputs({ ...listInputs, [index]: '' }); // Clear the input field for that index
+    }
+};
+// Function to update list input value for a specific heading index
+const handleListInputChange = (index, value) => {
+    setListInputs({ ...listInputs, [index]: value }); // Update the listInputs state for the corresponding index
+};
+const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
   
 
   return (
@@ -38,8 +56,31 @@ const TodoList = () => {
         <h3>{todo.heading}</h3> {/* Display the heading here */}
     {/* Button to delete the current heading by passing its index */}
         <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+
+        <ul>
+     {/* Iterate over each list item inside the current todo */}
+     {todo.lists.map((list, listIndex) => (
+       <li key={listIndex} className='todo_inside_list'>
+         {/* Display the text content of the list item */}
+         <p>{list}</p>
+       </li>
+     ))}
+   </ul>
+        <div className='add_list'>
+{/* Input field for adding a new item under a specific heading */}
+          <input
+            type="text"
+            className="list-input"
+            placeholder="Add List"
+            value={listInputs[index] || ''}// Use the value from listInputs array based on the current heading index
+            onChange={(e) => handleListInputChange(index, e.target.value)}/>
+ {/* Button to add the list item to the corresponding heading */}
+          <button className="add-list-button" onClick={() => handleAddList(index)}>Add List</button>
+        </div>
       </div>
     </div>
+
+
   ))}
         
       </div>
