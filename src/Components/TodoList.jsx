23872a8592be5e7a0f6547ab074cd2b4626e7c2a
vishnuvb5991-spec <vishnuvb5991@gmail.com>
2025-10-21 04:32:26 -0400

    updated todo-list

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+      const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+
+    const handleAddTodo = () => {
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
   
 
   return (
@@ -13,12 +23,24 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}}
             
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
+        {todos.map((todo, index) => ( // Iterate over each todo item in the todos array
+    <div key={index} className="todo-card">
+      <div className="heading_todo">
+    {/* Display the heading text of the current todo item */}
+        <h3>{todo.heading}</h3> {/* Display the heading here */}
+    {/* Button to delete the current heading by passing its index */}
+        <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+      </div>
+    </div>
+  ))}
         
       </div>
     </>
